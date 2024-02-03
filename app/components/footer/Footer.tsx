e3960342cfac659802eb7e53d91c5df29d2c7619
Katherine Chao <me@katherinechao.com>
2024-02-02 21:52:10 -0800

    styled footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import Container from "../Container";
+import { FaInstagram } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+            <Container>
+                <div className="w-full py-10 flex gap-2 justify-center text-2xl">
+                    <Link href="#">
+                        <MdOutlineEmail />
+                    </Link>
+                    <Link href="#">
+                        <FaInstagram />
+                    </Link>
+                </div>
+            </Container>
+        </footer>
+    );
 }
 
 export default Footer;
