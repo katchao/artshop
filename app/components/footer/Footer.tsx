9941cba8b5a54cf66dd434a551ebd055efdc156b
Katherine Chao <me@katherinechao.com>
2024-02-03 16:08:34 -0800

    styled footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -7,13 +7,20 @@ function Footer() {
     return (
         <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
             <Container>
-                <div className="w-full py-10 flex gap-2 justify-center text-2xl">
-                    <Link href="#">
-                        <MdOutlineEmail />
-                    </Link>
-                    <Link href="#">
-                        <FaInstagram />
-                    </Link>
+                <div className="w-full py-10 flex gap-2 justify-around text-1xl">
+                    <div>
+                        {/* TODO: Replace with actual links */}
+                        {/* TODO: Make FAQ Page */}
+                        <Link href="#">FAQ</Link>
+                    </div>
+                    <div className="flex flex-row gap-1">
+                        <Link href="#">
+                            <MdOutlineEmail />
+                        </Link>
+                        <Link href="#">
+                            <FaInstagram />
+                        </Link>
+                    </div>
                 </div>
             </Container>
         </footer>
